57b9279c1e51588da6ef342f7bf33d616e06ebb7
akcaydogu <akcay.dogukan61@gmail.com>
2024-10-16 11:32:29 +0300

    Update

--- src/Amazon/Module2/AmazonLab_1.java
+++ src/Amazon/Module2/AmazonLab_1.java
@@ -8,25 +8,19 @@ public class AmazonLab_1 {
 
         Scanner scanner = new Scanner(System.in);
 
-//        System.out.println("Please enter a flavor you want for ice cream.");
-//        String flavor = scanner.nextLine();
-        String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
-        for(int i = 0; i<cars.length; i++) {
-            System.out.println(cars[i]);
-        }
-        for(String meyve: cars) {
-            System.out.println(meyve);
-        }
+        double period, length, gravity;
 
-//        switch(flavor) {
-//            case "Strawberry":
-//                System.out.println("Strawberry is good choice");
-//                break;
-//            case "Vanilla":
-//                System.out.println("Vanilla is good choice");
-//
-//
-//        }
+        System.out.println("Enter period: ");
+        period = scanner.nextDouble();
+        System.out.println("Enter length: ");
+        length = scanner.nextDouble();
+        System.out.println("Enter gravity: ");
+        gravity = scanner.nextDouble();
+
+        period = 2*Math.PI*Math.sqrt(length/gravity);
+
+        System.out.println("Period: " + period);
+
 
 
     }
--- src/Ticket.java
+++ src/Ticket.java
@@ -9,17 +9,17 @@ public class Ticket {
         double subscriptionRemaining = 200d;
         double ticketBinPrice = 9.76d;
         double ticketAktPrice = 4.28d;
-        double total = 200;
+        double extraPrice = 0d;
 
         System.out.println("Lütfen Günlük biniş sayınızı giriniz : ");
-        int ticketBin = scanner.nextInt()*30;
+        int ticketBin = scanner.nextInt();
         System.out.println("Lütfen Günlük aktarma sayısını giriniz : ");
-        int ticketAkt = scanner.nextInt()*30;
-        double totalPrice= (ticketBinPrice*ticketBin)+(ticketAktPrice*ticketAkt);
+        int ticketAkt = scanner.nextInt();
+        double totalPrice= (ticketBinPrice*ticketBin)*30+(ticketAktPrice*ticketAkt)*30;
         
-        if(ticketAkt+ticketBin*2 < subscriptionRemaining) {
-            System.out.println("Sadece " + (ticketAkt+ticketBin*2) + " hakkı kullandınız" );
-            System.out.println("Kalan Abonman Hakkınız "+ (int)(subscriptionRemaining-(ticketAkt+ticketBin*2)) +"'dır");
+        if((ticketAkt+ticketBin*2)*30 < subscriptionRemaining) {
+            System.out.println("Sadece " + ((ticketAkt+ticketBin*2)*30) + " hakkı kullandınız" );
+            System.out.println("Kalan Abonman Hakkınız "+ (int)(subscriptionRemaining-(ticketAkt+ticketBin*2)*30) +"'dır");
             if(subscriptionPrice > totalPrice){
                 System.out.println("Akbil basmak daha mantıklı " + (float)(subscriptionPrice-totalPrice) + "TL kâr");
             } else if (subscriptionPrice < totalPrice){
@@ -28,30 +28,22 @@ public class Ticket {
             } else {
                 System.out.println("Keyfine göre 🤓");
             }
-        } else if(ticketAkt+ticketBin*2 > subscriptionRemaining) {
-            for (int i = 1; i<=30; i++) {
-                if ( total <= 200) {
-                    total -= ticketBin/30;
-                    ticketBin -= ticketBin/30;
-                } else {
-                    break;
-                }
-                if ( total <= 200) {
-                    total -= ticketAkt/30;
-                    ticketAkt -= ticketAkt/30;
-                } else {
+        } else {
+            for ( int i = 1; i <= 30; i++) {
+                subscriptionRemaining -= ticketBin*2 + ticketAkt;
+                System.out.println(subscriptionRemaining);
+                if (subscriptionRemaining <= 0) {
+                    ticketBin *= 30-i;
+                    ticketAkt *= 30-i;
+                    extraPrice = (ticketAktPrice+ticketBinPrice)/2*Math.abs(subscriptionRemaining);
                     break;
                 }
             }
-            System.out.println("Kalan akt" + ticketAkt + "Kalana bin" + ticketBin);
+            System.out.println(ticketBin);
+            System.out.println(ticketAkt);
+            System.out.println("Tüm abonman hakkınızı bitirdiniz");
+            System.out.println("Toplam harcamanız 200 abonman hakkı ve " + (ticketBin*ticketBinPrice+ticketAkt*ticketAktPrice+extraPrice) + " TL harcamanız bulunmaktadır");
         }
-
-
-
-
-
-
-
 
     }
 
